6bad7f1471173ba7d3318106a0f2bc5aa0efb7b8
Debbie Johnson <deborahlib@gmail.com>
2023-01-26 10:17:19 -0700

    correct error in js file name

--- jss/scripts.js
+++ jss/scripts.js
@@ -2,3 +2,5 @@ window.onload = function() {
   const form = document.querySelector("form");
   form.onsubmit = function(event) {
     event.preventDefault();
+
+let sql =document.getElementById("sql")    
